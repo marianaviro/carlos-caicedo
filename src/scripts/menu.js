9fc478ad921400371e3ba3af14e5d5edc577e270
Mariana Villamizar <marianaviro93@gmail.com>
2023-04-19 00:38:08 -0500

    Home and navigation in progress

---
+++ src/scripts/menu.js
@@ -0,0 +1,24 @@
+document.querySelector(".hamburger").addEventListener("click", () => {
+  //Make background unscrollable
+  document.querySelector(".content").classList.toggle("unscrollable");
+
+  //Rotate toggle icon
+  document.querySelector(".toggle").classList.toggle("rotated");
+
+  //Toggle nav container
+  if (document.querySelector(".nav-container").classList.contains("expanded")) {
+    document.querySelector(".nav-container").classList.remove("expanded");
+    document.querySelector(".nav-container").classList.add("collapsed");
+  } else {
+    document.querySelector(".nav-container").classList.remove("collapsed");
+    document.querySelector(".nav-container").classList.add("expanded");
+  }
+});
+
+document.querySelector(".dropdown-toggle").addEventListener("click", () => {
+  //Change text color
+  document.querySelector(".dropdown-toggle").classList.toggle("text-pink");
+
+  //Toggle dropdown container
+  document.querySelector(".dropdown").classList.toggle("hidden");
+});
---
+++ tailwind.config.cjs
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        black: "#191919",
+        yellow: "#ffe200",
+        orange: "#fc5e02",
+        pink: "#ff74df",
+        white: "#f8f7f1",
+      },
+      fontFamily: {
+        sans: ['"Arial Narrow"', "Arial", "sans-serif"],
+        display: ["Gregor", "Impact", "sans-serif"],
+        serif: ["Bagatela", "Times New Roman", "serif"],
+      },
+      fontSize: {
+        xs: "0.85rem",
+        xxs: "0.7rem",
+        "full-mobile": "6.3rem",
+        "full-desktop": "12rem",
+      },
+      dropShadow: {
+        pink: "0px 0px 8px #FF74E0",
+        orange: "0px 0px 5px #fc5e02",
+        "white-sm": "0px 0px 1px #f8f7f1",
+        "white-md": "0px 0px 3px rgba(246, 245, 239, 0.8)",
+      },
+      borderWidth: {
+        1: "1px",
+      },
+      opacity: {
+        85: ".85",
+      },
+      lineHeight: {
+        "very-tight": "0.8",
+        "extra-tight": "0.75",
+      },
+    },
+  },
+  plugins: [],
+};
